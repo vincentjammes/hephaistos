fd44ea852d353edc07c1b7df8c49c6b4e8e51bfe
Vincent Jammes <vincent@wisembly.com>
2015-08-18 23:50:11 +0200

    Dice | New CSS dice

--- dice/assets/main.js
+++ dice/assets/main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+var value = $('#input-dice-selector').value();
+// Comment valider le score
+var howMuch = function {
+	var diceNumber = document.getElementById("hu").value; 
+};
+
+var dice = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
+
+
+var rolls = [];
+
+for (var i = 1; i <= 1;  i++) {
+	rolls.push(value);
+};
