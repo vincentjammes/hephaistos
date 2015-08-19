11fb50d6b186dd71c7e88d90a73435286dc9c198
Vincent Jammes <vincent@wisembly.com>
2015-08-19 14:10:51 +0200

    Index | Fix Button link

--- dice/assets/main.js
+++ dice/assets/main.js
@@ -1,14 +1,5 @@
-var value = $('#input-dice-selector').value();
-// Comment valider le score
-var howMuch = function {
-	var diceNumber = document.getElementById("hu").value; 
+function getDice() {
+	document.getElementById("form1").submit();
 };
 
-var dice = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
-
-
-var rolls = [];
-
-for (var i = 1; i <= 1;  i++) {
-	rolls.push(value);
-};
+console.log(getDice)
