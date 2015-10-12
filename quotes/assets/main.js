96a89d83b5e482ec4e250c10b91cc6a869a4954d
Vincent Jammes <vincent@wisembly.com>
2015-10-12 13:25:09 +0200

    Quotes | New panel

--- quotes/assets/main.js
+++ quotes/assets/main.js
@@ -2,54 +2,77 @@ function getQuotes() {
     var quotes = [
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-FightClub-golem13.jpg",
-            text : "C'est quand on a tout perdu qu'on est libre de faire ce que l'on veut."
+            text : "C'est quand on a tout perdu qu'on est libre de faire ce que l'on veut.",
+            title : "Fight Club"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-IntoTheWild-golem13.jpg",
-            text : "Le bonheur n'est r&eacute;el que lorsqu'il est partag&eacute;." 
+            text : "Le bonheur n'est r&eacute;el que lorsqu'il est partag&eacute;.",
+            title : "Into the Wild"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-leParrain2-golem13.jpg",
-            text : "Gardez vos amis pr&egrave;s de vous, mais gardez vos ennemis encore plus pr&egrave;s"
+            text : "Gardez vos amis pr&egrave;s de vous, mais gardez vos ennemis encore plus pr&egrave;s",
+            title : "Le Parrain 2"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-LordOfWar-golem3-2.jpg",
-            text :"Rien ne co&ucirc;te plus cher &agrave; un marchand d'arme que la paix."
+            text :"Rien ne co&ucirc;te plus cher &agrave; un marchand d'arme que la paix.",
+            title : "Lord of War"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-PamelaRose-golem13.jpg",
-            text : "Il a &eacute;t&eacute; &eacute;lectrocut&eacute; pour avoir mang&eacute; des abats."
+            text : "Il a &eacute;t&eacute; &eacute;lectrocut&eacute; pour avoir mang&eacute; des abats.",
+            title : "Mais qui a tué Pamela Rose ?"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-PortesDisparus3-golem13.jpg",
-            text : "Je mets les pieds o&ugrave; je veux et c'est souvent dans la gueule."
+            text : "Je mets les pieds o&ugrave; je veux et c'est souvent dans la gueule.",
+            title : "Portés Disparus 3"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-BlairWitch-golem13.jpg",
-            text : "J'ai peur d'ouvrir les yeux. J'ai peur de les fermer ..."
+            text : "J'ai peur d'ouvrir les yeux. J'ai peur de les fermer ...",
+            title : "Le Projet Blair Witch"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-Brice-golem13.jpg",
-            text : 'Tu es comme le "H" de Hawa&iuml;, tu ne sers &agrave; rien !'
+            text : 'Tu es comme le "H" de Hawa&iuml;, tu ne sers &agrave; rien !',
+            title : "Brice de Nice"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-RRRRrrr-golem13.jpg",
-            text : "Elle est morte de vieillesse ... mais jeune."
+            text : "Elle est morte de vieillesse ... mais jeune.",
+            title : "RRRRrrr"
         },
         {
             picture : "http://golem13.fr/wp-content/uploads/2013/05/citaffiches-Usual-Suspects-golem13.jpg",
-            text : "On peut convaincre tout le monde qu'on a chang&eacute; mais jamais soi-m&ecirc;me."
+            text : "On peut convaincre tout le monde qu'on a chang&eacute; mais jamais soi-m&ecirc;me.",
+            title : "Usual Suspects"
         }
     ];
     
     var whichQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    // document.getElementById("phrase").innerHTML = whichText ;
     $('#phrase').html(whichQuote.text);
     $('#picture').attr('src', whichQuote.picture);
+    $('#title').html(whichQuote.title);
 };
 
-$(document).ready(function() {
-    $(".pull-me").click(function() {
-        $(".panel").slideToggle('slow');
-    });    
-});
+var body = document.getElementsByTagName("body");
+
+ document.getElementById("js-LaunchRightPanel").onclick = function () {
+   document.getElementById("js-RightPanel").setAttribute("aria-hidden", "false");
+   document.getElementById("js-RightPanelOverlay").setAttribute("aria-hidden", "false");
+ }
+
+ document.getElementById("js-CloseRightPanel").onclick = function () {
+   document.getElementById("js-RightPanel").setAttribute("aria-hidden", "true");
+   document.getElementById("js-RightPanelOverlay").setAttribute("aria-hidden", "true");
+ }
+
+
+
+
+
+
+
